ed2cb598c7bda19a4617404e685ad9182dd1e9ec
cazuli <56639557+cazuli@users.noreply.github.com>
2020-12-11 15:23:05 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,155 @@
+import pygame
+#inicializando a biblioteca
+pygame.init()
+
+#inicializando a tela
+screen = pygame.display.set_mode((900,600), 0)
+
+#cores para o desenho
+AMARELO = (255, 255, 0)
+PRETO = (0, 0, 0)
+VERDE = (0, 100, 0)
+MARROM = (150, 75, 0)
+AZULE = (0, 0, 139)
+
+class Cenario:
+    def __init__(self, tamanho, bat):
+        self.bat=bat
+        self.tamanho = tamanho
+        self.matriz = [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        ]
+    def calcular_regras(self, col_int, lin_int, col, lin):
+        if 0 <= col_int < 1 and 0 <= lin_int < 3:
+            if self.matriz[lin_int][col_int]!=0:
+                bat.aceitar_movimento()
+
+    def pintar_linha(self, tela, l, linha):
+        for c, coluna in enumerate(linha):
+            x = c * self.tamanho
+            y = l * self.tamanho
+            cor = PRETO
+            if coluna == 2:
+                cor = AZULE
+            pygame.draw.rect(tela, cor, (x, y, self.tamanho, self.tamanho), 0)
+            if coluna == 1:
+                pygame.draw.circle(tela, AMARELO, (x + self.tamanho/2, y + self.tamanho/2), self.tamanho // 10.0)
+            if coluna == 3:
+                cor = VERDE
+                pygame.draw.rect(tela, cor, (x, y, self.tamanho, self.tamanho), 0)
+
+    def pintar(self, tela):
+        for l, linha in enumerate(self.matriz):
+            self.pintar_linha(tela, l, linha)
+
+
+#todas as informações do pacman estarão nesta classe
+class Morcego:
+    #construtor
+    def __init__(self, tamanho):
+        #posição que será desenhado
+        self.coluna = 0
+        self.linha = 1
+        self.coluna_intencao = self.coluna
+        self.linha_intencao = self.linha
+        self.centro_x = 400
+        self.centro_y = 300
+        self.tamanho = tamanho
+        self.raio = int(self.tamanho / 2)
+        self.mode = 1
+        #velocidade
+        self.vel_x = 0
+        self.vel_y = 0
+
+
+    def calcular_regras(self):
+        #self.coluna = self.coluna + self.vel_x
+        #self.linha = self.linha + self.vel_y
+        self.coluna_intencao = self.coluna+self.vel_x
+        self.linha_intencao = self.linha+self.vel_y
+        self.centro_x = int(self.coluna * self.tamanho + self.raio)
+        self.centro_y = int(self.linha * self.tamanho + self.raio)
+
+    def aceitar_movimento(self):
+        self.coluna=self.coluna_intencao
+        self.linha = self.linha_intencao
+
+    def pintar(self, tela):
+        # corpo
+        pygame.draw.circle(tela, MARROM, (self.centro_x, self.centro_y), self.raio, 0)
+
+    def processar_eventos(self, eventos):
+        for e in eventos:
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_RIGHT:
+                    self.vel_x = 1
+                    self.mode = 1
+                if e.key == pygame.K_LEFT:
+                    self.vel_x = -1
+                    self.mode = 2
+                if e.key == pygame.K_UP:
+                    self.vel_y = -1
+                    self.mode = 3
+                if e.key == pygame.K_DOWN:
+                    self.vel_y = 1
+                    self.mode = 4
+            elif e.type == pygame.KEYUP:
+                if e.key == pygame.K_RIGHT:
+                    self.vel_x = 0
+                if e.key == pygame.K_LEFT:
+                    self.vel_x = 0
+                if e.key == pygame.K_UP:
+                    self.vel_y = 0
+                if e.key == pygame.K_DOWN:
+                    self.vel_y = 0
+
+if __name__ == "__main__":
+    size = 600 // 30
+    bat = Morcego(size*10)
+    cenario = Cenario(size, bat)
+    while True:
+        # Calcular as Regras
+        bat.calcular_regras()
+        cenario.calcular_regras(bat.coluna_intencao, bat.linha_intencao, bat.coluna, bat.linha)
+        # Pintar a tela
+        screen.fill(PRETO)
+        cenario.pintar(screen)
+        bat.pintar(screen)
+        pygame.display.update()
+        pygame.time.delay(100)
+
+        #Captura de eventos
+        eventos = pygame.event.get()
+        for e in eventos:
+            if e.type == pygame.QUIT:
+                exit()
+        bat.processar_eventos(eventos)
